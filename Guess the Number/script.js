44a07f0e4a2711f6faa7dd0fe6cdb4de18ed47ab
Fitz Angelo <91787757+kulaizki@users.noreply.github.com>
2023-01-26 22:42:43 +0800

    opacity

--- Guess the Number/script.js
+++ Guess the Number/script.js
@@ -1,8 +1,13 @@
 // in progress
 const title = document.getElementById("title");
 const play = document.getElementById("play-card-container");
+const game = document.getElementsByClassName(".game-container");
 
-play.addEventListener("click", function(){moveUp(play)});
+play.addEventListener("click", () => {
+    moveUp(play);
+});
+
+show();
 
 function moveUp(divId) {
     let div = divId;
@@ -10,4 +15,16 @@ function moveUp(divId) {
     if (div.offsetTop > -div.clientHeight) {
         setTimeout(function(){moveUp(divId)}, 8);
     }
+}
+
+let int = setInterval(show, 30);
+let load = 0;
+function show() {
+    load++;
+    if (load > 99) { clearInterval(int); }
+    game.style.opacity = scale(load, 0, 100, 0, 1);
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 }
